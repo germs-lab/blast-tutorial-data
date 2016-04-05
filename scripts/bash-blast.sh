4c5979d069bf3bbf949f9a19a8f3e8bc3e335f3f
Adina <adina@iastate.edu>
2016-04-05 12:55:58 +0100

    added parser

--- scripts/bash-blast.sh
+++
@@ -1,3 +0,0 @@
-cd /home/ubuntu/bodega-howe-ncbi
-formatdb -i data/nifh-database-fastas/all-nifh.fa -o T -p F
-for x in data/*fa; do blastall -i $x -d data/nifh-database-fastas/all-nifh.fa =o $x.x.nifhblastnout -p blastn -p blastn -a 4 -b 1 -e 1-5 -m 8; done
--- scripts/clean-up.py
+++
@@ -1,6 +0,0 @@
-import screed, sys
-
-for record in screed.open(sys.argv[1]):
-    if 'nifh' in record.name.lower():
-        print ">" + record.name.split(' ')[0]
-        print record.sequence
